9cfc587d33cc08c54b522fdd54a416a7ec08ced5
Antinegr <artyomhovhannisyan.ait@gmail.com>
2021-07-19 22:04:25 +0400

    change page to project page

--- src/components/InProgressProjects/InProgressProjects.jsx
+++ src/components/InProgressProjects/InProgressProjects.jsx
@@ -3,36 +3,42 @@ import Menu from "../../pages/Home/Menu";
 
 import "../../css/InProgressProjects/in-progress-projects.css";
 
-export default function InProgressProjects({ pageInfo }) {
+export default function InProgressProjects({ pageInfo ,history}) {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
-    <div className={("in-progress-projects", showMenu ? "in-progress-projects-hide" : "")}>
+    <div
+      className={
+        ("in-progress-projects", showMenu ? "in-progress-projects-hide" : "")
+      }
+    >
       {showMenu && <Menu closeMenu={setShowMenu} />}
       <div className="in-progress-projects-header">
         <div
           className="menu-icon menu-black-icon"
           onClick={() => setShowMenu(!showMenu)}
-        >
-        </div>
-        <div className="header-logo">
+        ></div>
+        <div className="header-logo black-logo">
           <h3>Anarch</h3>
         </div>
       </div>
       <div className="in-progres-projects-content">
-        <h3 className="page-name">{pageInfo.pageName}-In-Progres-Projects</h3>
+        <h3 className="page-name">
+          {pageInfo.pageName}
+          <span className="page-name-bold-text"> - In Progress</span>
+        </h3>
         <div className="projects">
           {pageInfo.items.map((project, i) => {
             return (
-              <div className="project" key={i}>
-                <img src={project.url} alt="in-progres" key={i + "img"} />
-                <p key={i + "title"}>{project.projectTitle}</p>
+              <div className="project" key={i} onClick={() => history.push("/project")}>
+                <img src={project.url} alt="in-progres"/>
+                <p>{project.projectTitle}</p>
               </div>
             );
           })}
         </div>
       </div>
-      <div className="footer project-footer">
+      <div className="projects-footer">
         <div className="footer-social-media-icons">
           <img src="../images/facebook-logo.png" alt="facebook" />
           <img src="../images/instagram-logo.png" alt="instagram" />
@@ -40,7 +46,11 @@ export default function InProgressProjects({ pageInfo }) {
         <div className="footer-languages">
           <ul>
             <li>
-              <img src="../images/language.png" alt="languages" />
+              <img
+                src="../images/language.png"
+                alt="languages"
+                className="language-icon"
+              />
               <p>English</p>
               <img
                 src="../images/arrow-down.png"
--- src/components/SelectedProjects/SelectedProjects.jsx
+++ src/components/SelectedProjects/SelectedProjects.jsx
@@ -4,7 +4,7 @@ import Menu from "../../pages/Home/Menu";
 
 import "../../css/SelectedProjects/selected-projects.css";
 
-export default function SelectedProjects({ pageInfo }) {
+export default function SelectedProjects({ pageInfo, history }) {
   const [showMenu, setShowMenu] = useState(false);
 
   return (
@@ -19,24 +19,27 @@ export default function SelectedProjects({ pageInfo }) {
           className="menu-icon menu-black-icon"
           onClick={() => setShowMenu(!showMenu)}
         ></div>
-        <div className="header-logo">
+        <div className="header-logo black-logo">
           <h3>Anarch</h3>
         </div>
       </div>
       <div className="selected-projects-content">
-        <h3 className="page-name">{pageInfo.pageName}-Selected-Projects</h3>
+        <h3 className="page-name">
+          {pageInfo.pageName}
+          <span className="page-name-bold-text"> - Selected-Projects</span>
+        </h3>
         <div className="projects">
           {pageInfo.items.map((project, i) => {
             return (
-              <div className="project" key={i}>
-                <img src={project.url} alt="selected" key={i + "img"} />
-                <p key={i + "title"}>{project.projectTitle}</p>
+              <div className="project" key={i} onClick={() => history.push("/project")}>
+                <img src={project.url} alt="selected" />
+                <p>{project.projectTitle}</p>
               </div>
             );
           })}
         </div>
       </div>
-      <div className="footer projects-footer">
+      <div className="projects-footer">
         <div className="footer-social-media-icons">
           <img src="../images/facebook-logo.png" alt="facebook" />
           <img src="../images/instagram-logo.png" alt="instagram" />
@@ -44,7 +47,11 @@ export default function SelectedProjects({ pageInfo }) {
         <div className="footer-languages">
           <ul>
             <li>
-              <img src="../images/language.png" alt="languages" />
+              <img
+                src="../images/language.png"
+                alt="languages"
+                className="language-icon"
+              />
               <p>English</p>
               <img
                 src="../images/arrow-down.png"

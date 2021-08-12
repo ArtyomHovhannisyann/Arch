4211f98d15a711fe4f2284756d360e3c3afa9269
rafayeljamalyan <77635096+rafayeljamalyan@users.noreply.github.com>
2021-08-12 20:06:27 +0400

    Set real host

--- src/lib/constants.jsx
+++ src/lib/constants.jsx
@@ -1,4 +1,4 @@
-export const generalUrl = "http://192.168.0.207:3008";
+export const generalUrl = "http://api.profalbyanarch.com";
 export const GET_PICTURES = `${generalUrl}/api/data/pictures`;
 export const GET_VIDEOS = `${generalUrl}/api/data/videos`;
 export const GET_TEAM = `${generalUrl}/api/data/team`;

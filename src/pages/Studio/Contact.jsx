f9129f47f1462746e520938cbbccfdc9e8b49385
Antinegr <artyomhovhannisyan.ait@gmail.com>
2021-08-13 17:00:15 +0400

    Contact Header Bug Fix

--- src/pages/Studio/Contact.jsx
+++ src/pages/Studio/Contact.jsx
@@ -5,42 +5,36 @@ import Footer from "../../components/Footer/Footer";
 import "../../css/Studio/contact.css";
 import { generalUrl } from "../../lib/constants";
 import { getContacts } from "../../lib/requests";
+import Header from "../../components/Header/Header";
 
 import Menu from "../Home/Menu";
 
 export default function Contact() {
   const [showMenu, setShowMenu] = useState(false);
-  const[image,setImage] = useState("")
+  const [image, setImage] = useState("");
   const [address, setAddress] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
   const [email, setEmail] = useState("");
   useEffect(() => {
-    getContacts((data)=>{
-      data.length > 0 && setAddress(data[0].address)
-      data.length > 0 && setPhoneNumber(data[0]["phone-number"])
-      data.length > 0 && setEmail(data[0].email)
-      data.length > 0 && setImage(`${generalUrl}/${data[0].image}`)
-    })
-  }, [])
+    getContacts((data) => {
+      data.length > 0 && setAddress(data[0].address);
+      data.length > 0 && setPhoneNumber(data[0]["phone-number"]);
+      data.length > 0 && setEmail(data[0].email);
+      data.length > 0 && setImage(`${generalUrl}/${data[0].image}`);
+    });
+  }, []);
   return (
     <div className={`studio-contact ${showMenu ? "studio-contact-hide" : ""}`}>
       {showMenu && <Menu closeMenu={setShowMenu} />}
-      <div className="studio-contact-header">
-        <div
-          className="menu-icon menu-black-icon contact-menu-icon"
-          onClick={() => setShowMenu(!showMenu)}
-        ></div>
-        <div className="header-logo contact-header-logo black-logo">
-          <h3>Anarch</h3>
-        </div>
-      </div>
+      <Header setShowMenu={setShowMenu} />
       <div className="studio-contact-content">
-        <div className="studio-contact-left-bar" style ={{background:`url(${image})`}}></div>
+        <div
+          className="studio-contact-left-bar"
+          style={{ background: `url(${image})` }}
+        ></div>
         <div className="studio-contact-right-bar">
           <span className="contact-info-header">Anarch Kedem Architect</span>
-          <p className = "contact-address">
-            {address}
-          </p>
+          <p className="contact-address">{address}</p>
           <p className="contact-info-email">
             <span className="contact-info-header">Studio Manger</span>:
             <a
@@ -61,7 +55,7 @@ export default function Contact() {
           </p>
         </div>
       </div>
-      <Footer showIcons = {false}/>
+      <Footer showIcons={false} />
     </div>
   );
 }

03c0545c44f2d4253b04394825cb446c2769bad3
Antinegr <artyomhovhannisyan.ait@gmail.com>
2021-08-13 16:58:07 +0400

    Header Bug fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,37 @@
-import React from "react";
-import "../../css/Header/header.css"
+import React,{useEffect, useState} from "react"; 
+import { Link } from "react-router-dom"; 
+import "../../css/Header/header.css";
 
-export default function Header({setShowMenu}) {
-  return (
-    <div className="header">
-      <div
-        className="menu-icon menu-black-icon"
-        onClick={() => setShowMenu(true)}
-      ></div>
-      <div className="header-logo team-header-logo">
+export default function Header({ setShowMenu }) {
+  const [headerState, setHeaderState] = useState(``);
+
+  const scrollEventForHeader = ()=>{
+    setHeaderState("header-with-bg")
+    if (window.scrollY == 0) {
+      setHeaderState("")
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll",scrollEventForHeader)
+    return () => {
+       window.removeEventListener("scroll",scrollEventForHeader)
+    }
+    
+    
+  }, []);
+  return ( 
+  <div className={`header ${headerState}`}   >
+    <div
+      className="menu-icon menu-black-icon"
+      onClick={() => setShowMenu(true)}
+    ></div>
+    <div className="header-logo team-header-logo">
+      <Link to="/home">
         <h3>Anarch</h3>
-      </div>
+      </Link>
     </div>
-  );
-}
+  </div>
+    );
+  }
+  

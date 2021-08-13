72424d33c13484043a6290ae01055df1711e7d91
Antinegr <artyomhovhannisyan.ait@gmail.com>
2021-08-13 16:59:57 +0400

    Jobs Bug fix

--- src/pages/Studio/Jobs.jsx
+++ src/pages/Studio/Jobs.jsx
@@ -4,13 +4,14 @@ import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
 
 import "../../css/Studio/jobs.css";
+import { generalUrl } from "../../lib/constants";
 import { getJobs } from "../../lib/requests";
 
 import Menu from "../Home/Menu";
 
 export default function Jobs() {
   const [showMenu, setShowMenu] = useState(false);
-  const [ config, setConfig ] = useState({});
+  const [config, setConfig] = useState({});
   const { t, i18n } = useTranslation();
   useEffect(() => {
     getJobs((data) => {
@@ -22,10 +23,17 @@ export default function Jobs() {
       {showMenu && <Menu closeMenu={setShowMenu} />}
       <Header setShowMenu={setShowMenu} />
       <div className="studio-jobs-content">
-        <div className="studio-jobs-left-bar"></div>
+        <div
+          className="studio-jobs-left-bar"
+          style={{ backgroundImage: `url(${generalUrl + "/" + config.image})` }}
+        ></div>
         <div className="studio-jobs-right-bar">
-          <h3>{i18n.language === "am" ? config.title_hy: config.title  }</h3>
-          <p>{i18n.language === "am" ?config.description_hy: config.description}</p>
+          <h3>{i18n.language === "am" ? config.title_hy : config.title}</h3>
+          <p>
+            {i18n.language === "am"
+              ? config.description_hy
+              : config.description}
+          </p>
         </div>
       </div>
       <Footer showIcons={false} />

562f0e1a884c86bb196fea68a181abb0972bd57a
Anti <artyomhovhannisyan.ait@gmail.com>
2021-11-25 16:39:36 +0400

    remove am font

--- src/App.js
+++ src/App.js
@@ -23,10 +23,11 @@ function App() {
   const { t, i18n } = useTranslation();
   useEffect(() => {
     document.cookie = `token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuYXJjaC5oZWxwQGdtYWlsLmNvbSIsInRva2VuIjoiJDJiJDEwJDVqYTRIS0xKanFXek9xWlVNV3ZTVGVqajJ4a0hzVkhrOHhCa0xlcGlkRGY3d0dDOWM4RFp5IiwiaWF0IjoxNjI3NzIyMDg1LCJleHAiOjE2Mjc3MjIzODV9.-_RPYrEYrSMMOJRv1sb9o_AyI1hjS3n2ZEdKbn70Hw8`;
-    document.documentElement.style.setProperty(
-      "--bac-font",
-      i18n.language === "am" ? "Arial" : "Browallia New"
-    );
+    //remove Am font
+    // document.documentElement.style.setProperty(
+    //   "--bac-font",
+    //   i18n.language === "am" ? "Arial" : "Browallia New"
+    // );
     document.documentElement.style.setProperty(
       "--font-size",
       i18n.language === "am" ? ".8vw" : "1.3vw"
@@ -49,8 +50,7 @@ function App() {
       if (i18n.language === `am`) {
         app.classList.add(`lang-am`);
         app.classList.remove(`lang-eng`);
-      }
-      else {
+      } else {
         app.classList.add(`lang-eng`);
         app.classList.remove(`lang-am`);
       }

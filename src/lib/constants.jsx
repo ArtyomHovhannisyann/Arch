b79f2ade17cb58a451484242dd44b8a22249655a
hacker <armen.demirchyan.ait@gmail.com>
2024-02-12 14:11:43 +0400

    fix: backend url

--- src/lib/constants.jsx
+++ src/lib/constants.jsx
@@ -1,9 +1,9 @@
-export const generalUrl = "https://api.profalbyanarch.com";
+export const generalUrl = "https://api.anarch.am";
 export const GET_PICTURES = `${generalUrl}/api/data/pictures`;
 export const GET_VIDEOS = `${generalUrl}/api/data/videos`;
 export const GET_TEAM = `${generalUrl}/api/data/team`;
 export const GET_MEMBERS = `${generalUrl}/api/data/members`;
 export const GET_JOBS = `${generalUrl}/api/data/jobs`;
 export const GET_CONTACT = `${generalUrl}/api/data/contacts`;
-export const GET_PROJECTS = (type,category)=>`${generalUrl}/api/data/projects?type=${type}&category=${category}`
-export const GET_PROJECT_BY_ID = (id)=>`${generalUrl}/api/data/projects/${id}`
+export const GET_PROJECTS = (type, category) => `${generalUrl}/api/data/projects?type=${type}&category=${category}`
+export const GET_PROJECT_BY_ID = (id) => `${generalUrl}/api/data/projects/${id}`
